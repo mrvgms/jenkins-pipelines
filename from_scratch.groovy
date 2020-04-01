14a37c12f008c6fc95575050f295c8267656d30c
Merve Gumuscibik <mgumuscibik@gmail.com>
2020-03-31 21:31:04 -0400

    +

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -11,20 +11,20 @@ node {
 	}
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@dev.merv3.com                 sudo yum install httpd -y
+		ssh centos@dev1.merv3.com                 sudo yum install httpd -y
 		"""
 	}
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@dev.merv3.com:/tmp
-		ssh centos@jdev.merv3.com                 sudo cp -r /tmp/index.html /var/www/html/
-		ssh centos@jdev.merv3.com                 sudo cp -r /tmp/style.css /var/www/html/
-		ssh centos@jdev.merv3.com				   sudo chown centos:centos /var/www/html/
-		ssh centos@jdev.merv3.com				   sudo chmod 777 /var/www/html/*
+		scp -r *  centos@dev1.merv3.com:/tmp
+		ssh centos@jdev1.merv3.com                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@jdev1.merv3.com                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@jdev1.merv3.com				   sudo chown centos:centos /var/www/html/
+		ssh centos@jdev1.merv3.com				   sudo chmod 777 /var/www/html/*
 		"""
 	}
 	stage("Restart web server"){
-		sh "ssh centos@jdev.merv3.com                 sudo systemctl restart httpd"
+		sh "ssh centos@jdev1.merv3.com                 sudo systemctl restart httpd"
 	}
 	stage("Slack"){
 		slackSend color: '#BADA55', message: 'Hello, World!'
