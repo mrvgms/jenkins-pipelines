203109b20c29e79803d1ba102c397dda070df9db
Merve Gumuscibik <mgumuscibik@gmail.com>
2020-03-31 21:27:31 -0400

    changes made

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -11,12 +11,12 @@ node {
 	}
 	stage("Install Prerequisites"){
 		sh """
-		ssh centos@jenkins_worker1.acirrustech.com                 sudo yum install httpd -y
+		ssh centos@jenkins_worker1.merv3.com                 sudo yum install httpd -y
 		"""
 	}
 	stage("Copy artifacts"){
 		sh """
-		scp -r *  centos@jenkins_worker1.acirrustech.com:/tmp
+		scp -r *  centos@jenkins_worker1.merv3.com:/tmp
 		ssh centos@jenkins_worker1.merv3.com                 sudo cp -r /tmp/index.html /var/www/html/
 		ssh centos@jenkins_worker1.merv3.com                 sudo cp -r /tmp/style.css /var/www/html/
 		ssh centos@jenkins_worker1.merv3.com				   sudo chown centos:centos /var/www/html/
