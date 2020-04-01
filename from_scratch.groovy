9aa8c4c88a84b0b76a2a5abf07265877891e7d2f
Merve Gumuscibik <mgumuscibik@gmail.com>
2020-03-31 21:23:01 -0400

    from scratch edited

--- from_scratch.groovy
+++ from_scratch.groovy
@@ -1,51 +1,32 @@
-node { 
-    properties([
-       // Below line sets "iscard Builds more than 5" 
-       buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
-       
-       // Below line triggers this job every minute
-       pipelineTriggers([pollSCM('* * * * *')])
-       ])
-
-
-
-
-    
-
-stage("Pull Repo"){ 
-
-git 'https://github.com/farrukh90/cool_website.git'
-
-} 
-
-stage("Install Prerequisites"){ 
-
-sh """
-sudo yum install httpd -y
-sudo cp -r * /var/www/html/
-sudo systemctl start httpd
-
-
-"""
-
-} 
-
-stage("Stage3"){ 
-
-echo "hello" 
-
-} 
-
-stage("Stage4"){ 
-
-echo "hello" 
-
-} 
-
-stage("Stage5"){ 
-
-echo "hello" 
-
-} 
-
-} 
+node {
+	properties([
+		// Below line sets "Discard Builds more than 5"
+		buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+		
+		// Below line triggers this job every minute
+		pipelineTriggers([pollSCM('* * * * *')])
+		])
+	stage("Pull Repo"){
+		git   'https://github.com/farrukh90/cool_website.git'
+	}
+	stage("Install Prerequisites"){
+		sh """
+		ssh centos@jenkins_worker1.acirrustech.com                 sudo yum install httpd -y
+		"""
+	}
+	stage("Copy artifacts"){
+		sh """
+		scp -r *  centos@jenkins_worker1.acirrustech.com:/tmp
+		ssh centos@jenkins_worker1.merv3.com                 sudo cp -r /tmp/index.html /var/www/html/
+		ssh centos@jenkins_worker1.merv3.com                 sudo cp -r /tmp/style.css /var/www/html/
+		ssh centos@jenkins_worker1.merv3.com				   sudo chown centos:centos /var/www/html/
+		ssh centos@jenkins_worker1.merv3.com				   sudo chmod 777 /var/www/html/*
+		"""
+	}
+	stage("Restart web server"){
+		sh "ssh centos@jenkins_worker1.merv3.com                 sudo systemctl restart httpd"
+	}
+	stage("Slack"){
+		slackSend color: '#BADA55', message: 'Hello, World!'
+	}
+}
